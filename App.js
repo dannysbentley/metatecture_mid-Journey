5734d55efe78ffa3dd6e6562a0f846fb3be52e0c
Danny Bentley <danny.b@dwp.com>
2023-02-28 09:07:20 +0700

    adding checkbox in javascript.

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 var subjectObject = {
   "Architecture": {
     "Modern minimalism": ["Residential", "Commercial"],
-    "Sculptural Biomimicry ": ["Residential", "Commercial"],
-    "Bootstrap5": ["Residential", "Commercial"],    
+    "Sculptural Biomimicry ": ["Residential", "Commercial"]   
   },
 
   "Interior Design": {
@@ -15,11 +14,28 @@ var subjectObject = {
   }
 }
 
+const list = ["Option 1", "Option 2", "Option 3", "Option 4"];
 
 window.onload = function(){
   var first = document.getElementById('first')
   var second = document.getElementById('second')
   var third = document.getElementById('third')
+  var container = document.getElementById('container');
+
+  //Test to list checkboxes.
+  list.forEach((option) => {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "Options";
+    checkbox.value = option;
+
+    const label = document.createElement("label");
+    label.appendChild(document, createTextNode(option));
+
+    container.appendChild(checkbox);
+    container.appendChild(label);
+    checkboxesContainer.appendChild(document.createElement("br"));
+  });
 
   for(var x in subjectObject){
       // console.log(x);
@@ -38,7 +54,6 @@ window.onload = function(){
           second.options[second.options.length] = new Option(y)
       }
   }
-
 
   second.onchange = function(){
       third.length = 1
